0a4518ea616f582f32cfb3cb3bec37e55459575d
alicend <a.taiki623@gmail.com>
2023-05-18 21:11:31 +0900

    create login page

---
+++ client/app/lookback/components/Auth.tsx
@@ -0,0 +1,140 @@
+import { useState, FormEvent } from "react"; 
+import { useRouter } from "next/router"; 
+import Cookie from "universal-cookie";
+import axios, { AxiosResponse, AxiosError } from 'axios';
+
+const cookie = new Cookie();
+
+interface ResponseData {
+  access: string;
+}
+
+export const Auth = () => {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLogin, setIsLogin] = useState(true);
+
+  const login = async () => {
+    try {
+      const response: AxiosResponse<ResponseData> = await axios.post(
+          `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/auth/jwt/create/`,
+          { email: email, password: password },
+          { headers: {
+              "Content-Type": "application/json"
+            }
+          }
+      );
+  
+      // axios will throw an error when the status is not in the range of 2xx
+      // so there is no need to check for 400 specifically
+      // the data property will contain the parsed JSON response body
+
+      // ログインに成功すればcookieをセットする
+      const options = { path: "/"};
+      // access_tokenは任意の名称
+      cookie.set("access_token", response.data.access, options);
+      router.push("/main-page");
+  } catch (err: any) {
+      // if the request is made and the server responds with a 
+      // status code that falls out of the range of 2xx an error is thrown
+      const error: AxiosError = err;
+      if (error.response && error.response.status === 400) {
+          alert("authentication failed");
+      } else {
+          alert(error);
+      }
+  }
+  };
+
+  const authUser = async (e: FormEvent) => {
+    // ページのリロードを防ぐ(submitイベントの本来の動作を止める)
+    e.preventDefault();
+
+    if (isLogin) {
+      login();
+    } else {
+      try {
+        await axios.post(
+          `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/auth/jwt/register/`,
+          { email: email, password: password },
+          { headers: {
+              "Content-Type": "application/json"
+            }
+          }
+        );
+        login();
+      } catch (err) {
+        alert(err);
+      }
+    }
+  };
+
+  return(
+    <>
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img className="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
+          {isLogin ? "ログイン" : "新規登録"}
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" onSubmit={authUser}>
+          <div>
+            <label htmlFor="email" className="block text-sm font-medium leading-6 text-white">Email</label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                value={email}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label htmlFor="password" className="block text-sm font-medium leading-6 text-white">Password</label>
+              <div className="text-sm mr-2">
+                <a href="#" className="font-semibold text-indigo-500 hover:text-indigo-400">パスワードを忘れた</a>
+              </div>
+            </div>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                value={password}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setPassword(e.target.value);
+                }}
+              />
+            </div>
+          </div>
+
+          <div>
+            <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+              {isLogin ? "Sign in" : "Sign up"}
+            </button>
+          </div>
+        </form>
+
+        <div className="mt-2 mr-2 text-right text-sm" >
+          <span onClick={() => setIsLogin(!isLogin)} className="cursor-pointer font-semibold leading-6 text-indigo-500 hover:text-indigo-400">
+            {isLogin ? "新規登録する" : "ログインする"}
+          </span>
+        </div>
+      </div>
+    </>
+  );
+};
---
+++ client/app/lookback/pages/main-page.tsx
@@ -0,0 +1,34 @@
+import { Layout } from "@/components/Layout"
+import Cookies from "universal-cookie"
+import { useRouter } from "next/router"; 
+
+const cookie = new Cookies();
+
+export default function MainPage() {
+  const router = useRouter();
+
+  const logout = () => {
+    // access-tokenは認証時に決めた任意の値
+    cookie.remove("access-token");
+    router.push("/");
+  };
+
+  return (
+    <Layout title="Main Page">
+      <svg
+        onClick={logout}
+        xmlns="http://www.w3.org/2000/svg"
+        className="mt-10 cursor-pointer h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        strokeWidth={2}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+      </svg>
+    </Layout>
+  )
+}
