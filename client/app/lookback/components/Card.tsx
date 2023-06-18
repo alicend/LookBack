39011ae968f46e538cb3d51ca14a38cea2414c7d
alicend <a.taiki623@gmail.com>
2023-06-19 07:56:32 +0900

    タスクの変更モーダルをモーダルの外をクリックすると消える機能を追加

--- client/app/lookback/components/Card.tsx
+++ client/app/lookback/components/Card.tsx
@@ -1,5 +1,5 @@
 import { BiDotsHorizontalRounded } from "react-icons/bi";
-import { FC, useState, useCallback } from "react";
+import { FC, useState, useCallback, useEffect } from "react";
 import { DraggableItem } from "@/types/DraggableItem"; 
 
 type CardProps = {
@@ -8,16 +8,27 @@ type CardProps = {
 };
 
 export const Card: FC<CardProps> = ({ task, deleteTasks }) => {
-  const [show, setShow] = useState<boolean>(false);
-
-  const toggleShow = useCallback(() => {
-    setShow(prevShow => !prevShow);
-  }, []);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
   const handleDelete = useCallback(() => {
     deleteTasks(task);
-    setShow(false);
+    closeModal();
   }, [deleteTasks, task]);
+
+  const closeModal = useCallback(() => {
+    setIsModalOpen(false);
+    document.removeEventListener('click', closeModal);
+  }, []);
+
+  useEffect(() => {
+    return () => document.removeEventListener('click', closeModal);
+  }, [closeModal]);
+
+  const openModal = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.stopPropagation();
+    setIsModalOpen(true);
+    document.addEventListener('click', closeModal);
+  }
 
   return (
     <div className="flex cursor-move content-start items-start rounded-md border bg-white p-4">
@@ -25,10 +36,10 @@ export const Card: FC<CardProps> = ({ task, deleteTasks }) => {
         {task.contents}
       </p>
 
-      <button onClick={toggleShow}>
+      <button onClick={openModal}>
         <BiDotsHorizontalRounded className="h-4 w-4" />
-        {show && (
-          <div className="absolute z-50 list-none rounded border bg-white text-left text-sm">
+        {isModalOpen && (
+          <div onClick={closeModal} className="absolute z-50 list-none rounded border bg-white text-left text-sm">
             <ul>
               <li className="py-1 px-4 hover:bg-gray-100">
                 <div onClick={() => {}}>
