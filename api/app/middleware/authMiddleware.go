1b5a81a7fafb75c314c72127a0db5950cdd82e4c
alicend <a.taiki623@gmail.com>
2023-07-12 07:13:30 +0900

    認証エラー時にログを出力する機能を追加

--- api/app/middleware/authMiddleware.go
+++ api/app/middleware/authMiddleware.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -13,6 +14,8 @@ func AuthMiddleware(c *gin.Context) {
 	// トークンが含まれているか確認
 	tokenString, err := c.Cookie(constant.JWT_TOKEN_NAME)
 	if err != nil {
+		log.Printf("Error retrieving cookie: %v", err)
+		log.Printf("認証情報が存在しません")
 		c.JSON(http.StatusUnauthorized, gin.H{
 			"message": "Unauthorized",
 		})
@@ -23,6 +26,8 @@ func AuthMiddleware(c *gin.Context) {
 	// 正しいトークンか確認
 	_, err = utils.ParseToken(tokenString)
 	if err != nil {
+		log.Printf("Error parsing token: %v", err)
+		log.Printf("認証情報が正しくありません")
 		c.JSON(http.StatusUnauthorized, gin.H{
 			"message": "Invalid token",
 		})
