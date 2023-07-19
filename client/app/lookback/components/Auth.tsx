0d59535718d65d7dbab8f90de11838517fe45204
alicend <a.taiki623@gmail.com>
2023-07-19 20:03:27 +0900

    登録時とログイン時のヴァリデーション機能を追加

--- client/app/lookback/components/Auth.tsx
+++ client/app/lookback/components/Auth.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { z } from 'zod';
+
 import { styled } from '@mui/system';
 import { TextField, Button } from "@mui/material";
 
@@ -43,22 +45,52 @@ const StyledTextField = styled(TextField)(({ theme }) => ({
   },
 }));
 
+// 少なくとも1つの英字と1つの数字を含む
+const passwordCheck = (val: string) => /[A-Za-z].*[0-9]|[0-9].*[A-Za-z]/.test(val);
+
+const credentialSchema = z.object({
+  username: z.string(),
+  password: z.string()
+    .min(8, "パスワードは８文字以上にしてください")
+    .refine(passwordCheck, "パスワードには少なくとも１つ以上の半角英字と半角数字を含めてください"),
+});
+
 const Auth: React.FC = () => {
   const dispatch: AppDispatch = useDispatch();
   const isLoginView = useSelector(selectIsLoginView);
   const [credential, setCredential] = useState({ username: "", password: "" });
+  const [errors, setErrors] = useState({ username: "", password: "" });
+  const usernameError = errors.username;
+  const passwordError = errors.password;
+
+  const isDisabled =
+  credential.username.length === 0 ||
+  credential.password.length === 0;
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     const name = e.target.name;
     setCredential({ ...credential, [name]: value });
+    setErrors({ ...errors, [name]: "" });
   };
+  
   const login = async () => {
+
+    // 入力チェック
+    const result = credentialSchema.safeParse(credential);
+    if (!result.success) {
+      const usernameError = result.error.formErrors.fieldErrors["username"]?.[0] || "";
+      const passwordError = result.error.formErrors.fieldErrors["password"]?.[0] || "";
+      setErrors({ username: usernameError, password: passwordError });
+      return;
+    }
+
     if (isLoginView) {
+      // ログイン処理
       await dispatch(fetchAsyncLogin(credential));
     } else {
+      // 登録処理
       const result = await dispatch(fetchAsyncRegister(credential));
-      console.log(result);
       if (fetchAsyncRegister.fulfilled.match(result)) {
         await dispatch(fetchAsyncLogin(credential));
       }
@@ -78,6 +110,8 @@ const Auth: React.FC = () => {
         name="username"
         value={credential.username}
         onChange={handleInputChange}
+        error={Boolean(usernameError)}
+        helperText={usernameError}
       />
       <br />
       <StyledTextField
@@ -89,11 +123,14 @@ const Auth: React.FC = () => {
         name="password"
         value={credential.password}
         onChange={handleInputChange}
+        error={Boolean(passwordError)}
+        helperText={passwordError}
       />
       <StyledButton
         variant="contained"
         color="primary"
         size="small"
+        disabled={isDisabled}
         onClick={login}
       >
         {isLoginView ? "Login" : "Register"}
