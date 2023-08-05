95f54f8801388f5ae1b9c0b57ad7ea4848da3ef4
alicend <a.taiki623@gmail.com>
2023-08-05 18:25:43 +0900

    APIのタスクのEstimateにヴァリデーションを追加

--- api/app/models/task.go
+++ api/app/models/task.go
@@ -19,7 +19,7 @@ type Task struct {
 	Status            uint       `gorm:"not null"`
 	Responsible       uint       `gorm:"not null"`
 	ResponsibleUserID User       `gorm:"foreignKey:Responsible;"`
-	Estimate          *uint       `gorm:"not null"`
+	Estimate          *uint      `gorm:"not null" validate:"required,min=1,max=1000"`
 	StartDate         *time.Time `gorm:"not null"`
 }
 
@@ -27,7 +27,7 @@ type TaskInput struct {
 	Task        string `json:"Task" binding:"required,min=1,max=255"`
 	Description string `json:"Description" binding:"required,min=1,max=255"`
 	StartDate   string `json:"StartDate" binding:"required,min=1,max=24"`
-	Estimate    *uint   `json:"Estimate" binding:"required"`
+	Estimate    *uint   `json:"Estimate" binding:"required",,min=1,max=1000"`
 	Responsible uint   `json:"Responsible" binding:"required"`
 	Status      uint   `json:"Status" binding:"required"`
 	CategoryID  uint   `json:"Category" binding:"required"`
