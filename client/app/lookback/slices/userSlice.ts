e4eab11ddf526eb9ef956941a4990a2cfeef47c0
alicend <a.taiki623@gmail.com>
2023-10-03 21:31:34 +0900

    サーバーでエラーが起きた時にフロントでエラーにならないように修正

--- client/app/lookback/slices/userSlice.ts
+++ client/app/lookback/slices/userSlice.ts
@@ -43,14 +43,14 @@ export const fetchAsyncGuestLogin = createAsyncThunk(
     try {
       const res = await axios.get(
         `${ENDPOINTS.LOGIN}/guest`,
-        COMMON_HTTP_HEADER,
+        COMMON_HTTP_HEADER
       );
       await router.push("/task-board");
       return res.data;
     } catch (err: any) {
       return handleHttpError(err, thunkAPI);
     }
-  },
+  }
 );
 
 export const fetchAsyncLogin = createAsyncThunk(
@@ -58,12 +58,13 @@ export const fetchAsyncLogin = createAsyncThunk(
   async (auth: LOGIN_AUTH, thunkAPI) => {
     try {
       const res = await axios.post(ENDPOINTS.LOGIN, auth, COMMON_HTTP_HEADER);
+      await console.log(res);
       await router.push("/task-board");
       return res.data;
     } catch (err: any) {
       return handleHttpError(err, thunkAPI);
     }
-  },
+  }
 );
 
 export const fetchAsyncLogout = createAsyncThunk(
@@ -76,7 +77,7 @@ export const fetchAsyncLogout = createAsyncThunk(
     } catch (err: any) {
       return handleHttpError(err, thunkAPI);
     }
-  },
+  }
 );
 
 export const fetchAsyncRegisterRequest = createAsyncThunk(
@@ -86,32 +87,32 @@ export const fetchAsyncRegisterRequest = createAsyncThunk(
       const res = await axios.post(
         ENDPOINTS.REGISTER_REQUEST,
         { email: email },
-        COMMON_HTTP_HEADER,
+        COMMON_HTTP_HEADER
       );
       return res.data;
     } catch (err: any) {
       return handleHttpError(err, thunkAPI);
     }
-  },
+  }
 );
 
 export const fetchAsyncInviteRequest = createAsyncThunk(
   "auth/invite/request",
   async (
     { email, userGroupID }: { email: string; userGroupID: number },
-    thunkAPI,
+    thunkAPI
   ) => {
     try {
       const res = await axios.post(
         ENDPOINTS.INVITE_REQUEST,
         { email: email, userGroupID: userGroupID },
-        COMMON_HTTP_HEADER,
+        COMMON_HTTP_HEADER
       );
       return res.data;
     } catch (err: any) {
       return handleHttpError(err, thunkAPI);
     }
-  },
+  }
 );
 
 export const fetchAsyncRegister = createAsyncThunk(
@@ -121,14 +122,14 @@ export const fetchAsyncRegister = createAsyncThunk(
       const res = await axios.post(
         ENDPOINTS.REGISTER,
         auth,
-        COMMON_HTTP_HEADER,
+        COMMON_HTTP_HEADER
       );
       await router.push("/");
       return res.data;
     } catch (err: any) {
       return handleHttpError(err, thunkAPI);
     }
-  },
+  }
 );
 
 export const fetchAsyncInviteRegister = createAsyncThunk(
@@ -139,14 +140,14 @@ export const fetchAsyncInviteRegister = createAsyncThunk(
       const res = await axios.post(
         ENDPOINTS.INVITE_REGISTER,
         auth,
-        COMMON_HTTP_HEADER,
+        COMMON_HTTP_HEADER
       );
       await router.push("/");
       return res.data;
     } catch (err: any) {
       return handleHttpError(err, thunkAPI);
     }
-  },
+  }
 );
 
 export const fetchAsyncGetLoginUser = createAsyncThunk(
@@ -158,7 +159,7 @@ export const fetchAsyncGetLoginUser = createAsyncThunk(
     } catch (err: any) {
       return handleHttpError(err, thunkAPI);
     }
-  },
+  }
 );
 
 export const fetchAsyncUpdateLoginUserEmail = createAsyncThunk(
@@ -168,13 +169,13 @@ export const fetchAsyncUpdateLoginUserEmail = createAsyncThunk(
       const res = await axios.put(
         `${ENDPOINTS.USERS}/me/email/request`,
         { email: email },
-        COMMON_HTTP_HEADER,
+        COMMON_HTTP_HEADER
       );
       return res.data.user;
     } catch (err: any) {
       return handleHttpError(err, thunkAPI);
     }
-  },
+  }
 );
 
 export const fetchAsyncUpdateCompleteLoginUserEmail = createAsyncThunk(
@@ -184,14 +185,14 @@ export const fetchAsyncUpdateCompleteLoginUserEmail = createAsyncThunk(
       const res = await axios.put(
         `${ENDPOINTS.USERS}/me/email`,
         { email: email },
-        COMMON_HTTP_HEADER,
+        COMMON_HTTP_HEADER
       );
       await router.push("/");
       return res.data.user;
     } catch (err: any) {
       return handleHttpError(err, thunkAPI);
     }
-  },
+  }
 );
 
 export const fetchAsyncUpdateLoginUsername = createAsyncThunk(
@@ -201,13 +202,13 @@ export const fetchAsyncUpdateLoginUsername = createAsyncThunk(
       const res = await axios.put(
         `${ENDPOINTS.USERS}/me/name`,
         { username: username },
-        COMMON_HTTP_HEADER,
+        COMMON_HTTP_HEADER
       );
       return res.data.user;
     } catch (err: any) {
       return handleHttpError(err, thunkAPI);
     }
-  },
+  }
 );
 
 export const fetchAsyncUpdateLoginUserPassword = createAsyncThunk(
@@ -217,13 +218,13 @@ export const fetchAsyncUpdateLoginUserPassword = createAsyncThunk(
       const res = await axios.put(
         `${ENDPOINTS.USERS}/me/password`,
         passwords,
-        COMMON_HTTP_HEADER,
+        COMMON_HTTP_HEADER
       );
       return res.data.user;
     } catch (err: any) {
       return handleHttpError(err, thunkAPI);
     }
-  },
+  }
 );
 
 export const fetchAsyncResetPasswordRequest = createAsyncThunk(
@@ -233,13 +234,13 @@ export const fetchAsyncResetPasswordRequest = createAsyncThunk(
       const res = await axios.post(
         `${ENDPOINTS.USERS}/password/request`,
         { email: email },
-        COMMON_HTTP_HEADER,
+        COMMON_HTTP_HEADER
       );
       return res.data.user;
     } catch (err: any) {
       return handleHttpError(err, thunkAPI);
     }
-  },
+  }
 );
 
 export const fetchAsyncResetPassword = createAsyncThunk(
@@ -249,14 +250,14 @@ export const fetchAsyncResetPassword = createAsyncThunk(
       const res = await axios.put(
         `${ENDPOINTS.USERS}/password`,
         reset,
-        COMMON_HTTP_HEADER,
+        COMMON_HTTP_HEADER
       );
       await router.push("/");
       return res.data.user;
     } catch (err: any) {
       return handleHttpError(err, thunkAPI);
     }
-  },
+  }
 );
 
 export const fetchAsyncUpdateUserGroup = createAsyncThunk(
@@ -266,13 +267,13 @@ export const fetchAsyncUpdateUserGroup = createAsyncThunk(
       const res = await axios.put(
         `${ENDPOINTS.USERS}/me/user-group`,
         { userGroup: userGroup },
-        COMMON_HTTP_HEADER,
+        COMMON_HTTP_HEADER
       );
       return res.data.user_groups;
     } catch (err: any) {
       return handleHttpError(err, thunkAPI);
     }
-  },
+  }
 );
 
 export const fetchAsyncDeleteLoginUser = createAsyncThunk(
@@ -281,14 +282,14 @@ export const fetchAsyncDeleteLoginUser = createAsyncThunk(
     try {
       const res = await axios.delete(
         `${ENDPOINTS.USERS}/me`,
-        COMMON_HTTP_HEADER,
+        COMMON_HTTP_HEADER
       );
       await router.push("/");
       return res.data.user;
     } catch (err: any) {
       return handleHttpError(err, thunkAPI);
     }
-  },
+  }
 );
 
 const initialState: USER_STATE = {
@@ -308,21 +309,31 @@ const handleError = (state: any, action: any) => {
   if (payload.status === 401) {
     router.push("/");
   } else {
-    const errorMessage = payload.response.message
-      ? payload.response.message
-      : payload.response.error;
-    state.status = "failed";
-    state.message = errorMessage;
+    if (action.payload && action.payload.response) {
+      const errorMessage = action.payload.response.message
+        ? action.payload.response.message
+        : action.payload.response.error;
+      state.status = "failed";
+      state.message = errorMessage;
+    } else {
+      state.status = "failed";
+      state.message = "Unknown error";
+    }
   }
 };
 
 const handleLoginError = (state: any, action: any) => {
   const payload = action.payload as PAYLOAD;
-  const errorMessage = payload.response.message
-    ? payload.response.message
-    : payload.response.error;
-  state.status = "failed";
-  state.message = errorMessage;
+  if (action.payload && action.payload.response) {
+    const errorMessage = action.payload.response.message
+      ? action.payload.response.message
+      : action.payload.response.error;
+    state.status = "failed";
+    state.message = errorMessage;
+  } else {
+    state.status = "failed";
+    state.message = "Unknown error";
+  }
 };
 
 const handleLoading = (state: any) => {
@@ -343,7 +354,7 @@ export const userSlice = createSlice({
       (state, action: PayloadAction<USER>) => {
         state.loginUser = action.payload;
         state.message = "ログインに成功しました";
-      },
+      }
     );
     builder.addCase(fetchAsyncGuestLogin.rejected, handleLoginError);
     builder.addCase(fetchAsyncGuestLogin.pending, handleLoading);
@@ -352,7 +363,7 @@ export const userSlice = createSlice({
       (state, action: PayloadAction<USER>) => {
         state.loginUser = action.payload;
         state.message = "ログインに成功しました";
-      },
+      }
     );
     builder.addCase(fetchAsyncLogin.rejected, handleLoginError);
     builder.addCase(fetchAsyncLogin.pending, handleLoading);
@@ -362,7 +373,7 @@ export const userSlice = createSlice({
         state.status = "succeeded";
         state.loginUser = action.payload;
         state.message = "ログアウトしました";
-      },
+      }
     );
     builder.addCase(fetchAsyncLogout.rejected, handleLoginError);
     builder.addCase(fetchAsyncLogout.pending, handleLoading);
@@ -372,7 +383,7 @@ export const userSlice = createSlice({
         state.status = "succeeded";
         state.loginUser = action.payload;
         state.message = "メールを送信しました";
-      },
+      }
     );
     builder.addCase(fetchAsyncRegisterRequest.rejected, handleLoginError);
     builder.addCase(fetchAsyncRegisterRequest.pending, handleLoading);
@@ -382,7 +393,7 @@ export const userSlice = createSlice({
         state.status = "succeeded";
         state.loginUser = action.payload;
         state.message = "メールを送信しました";
-      },
+      }
     );
     builder.addCase(fetchAsyncInviteRequest.rejected, handleLoginError);
     builder.addCase(fetchAsyncInviteRequest.pending, handleLoading);
@@ -392,7 +403,7 @@ export const userSlice = createSlice({
         state.status = "succeeded";
         state.loginUser = action.payload;
         state.message = "ユーザーの登録に成功しました";
-      },
+      }
     );
     builder.addCase(fetchAsyncRegister.rejected, handleLoginError);
     builder.addCase(fetchAsyncRegister.pending, handleLoading);
@@ -403,7 +414,7 @@ export const userSlice = createSlice({
         state.status = "succeeded";
         state.loginUser = action.payload;
         state.message = "ユーザーの登録に成功しました";
-      },
+      }
     );
     builder.addCase(fetchAsyncInviteRegister.rejected, handleLoginError);
     builder.addCase(fetchAsyncInviteRegister.pending, handleLoading);
@@ -411,7 +422,7 @@ export const userSlice = createSlice({
       fetchAsyncGetLoginUser.fulfilled,
       (state, action: PayloadAction<USER>) => {
         state.loginUser = action.payload;
-      },
+      }
     );
     builder.addCase(fetchAsyncGetLoginUser.rejected, handleError);
     builder.addCase(fetchAsyncGetLoginUser.pending, handleLoading);
@@ -421,7 +432,7 @@ export const userSlice = createSlice({
         state.status = "succeeded";
         state.loginUser = action.payload;
         state.message = "メールを送信しました";
-      },
+      }
     );
     builder.addCase(fetchAsyncUpdateLoginUserEmail.rejected, handleError);
     builder.addCase(fetchAsyncUpdateLoginUserEmail.pending, handleLoading);
@@ -431,15 +442,15 @@ export const userSlice = createSlice({
         state.status = "succeeded";
         state.loginUser = action.payload;
         state.message = "メールの更新に完了しました";
-      },
+      }
     );
     builder.addCase(
       fetchAsyncUpdateCompleteLoginUserEmail.rejected,
-      handleError,
+      handleError
     );
     builder.addCase(
       fetchAsyncUpdateCompleteLoginUserEmail.pending,
-      handleLoading,
+      handleLoading
     );
     builder.addCase(
       fetchAsyncUpdateLoginUsername.fulfilled,
@@ -447,7 +458,7 @@ export const userSlice = createSlice({
         state.status = "succeeded";
         state.loginUser = action.payload;
         state.message = "ユーザー名の更新に成功しました";
-      },
+      }
     );
     builder.addCase(fetchAsyncUpdateLoginUsername.rejected, handleError);
     builder.addCase(fetchAsyncUpdateLoginUsername.pending, handleLoading);
@@ -457,7 +468,7 @@ export const userSlice = createSlice({
         state.status = "succeeded";
         state.loginUser = action.payload;
         state.message = "パスワードの更新に成功しました";
-      },
+      }
     );
     builder.addCase(fetchAsyncUpdateLoginUserPassword.rejected, handleError);
     builder.addCase(fetchAsyncUpdateLoginUserPassword.pending, handleLoading);
@@ -467,7 +478,7 @@ export const userSlice = createSlice({
         state.status = "succeeded";
         state.loginUser = action.payload;
         state.message = "メールを送信しました";
-      },
+      }
     );
     builder.addCase(fetchAsyncResetPasswordRequest.rejected, handleError);
     builder.addCase(fetchAsyncResetPasswordRequest.pending, handleLoading);
@@ -477,7 +488,7 @@ export const userSlice = createSlice({
         state.status = "succeeded";
         state.loginUser = action.payload;
         state.message = "パスワードをリセットしました";
-      },
+      }
     );
     builder.addCase(fetchAsyncResetPassword.rejected, handleError);
     builder.addCase(fetchAsyncResetPassword.pending, handleLoading);
@@ -487,7 +498,7 @@ export const userSlice = createSlice({
         state.status = "succeeded";
         state.loginUser = action.payload;
         state.message = "ユーザーグループの更新に成功しました";
-      },
+      }
     );
     builder.addCase(fetchAsyncUpdateUserGroup.rejected, handleError);
     builder.addCase(fetchAsyncUpdateUserGroup.pending, handleLoading);
@@ -497,7 +508,7 @@ export const userSlice = createSlice({
         state.status = "succeeded";
         state.loginUser = action.payload;
         state.message = "ユーザーの削除に成功しました";
-      },
+      }
     );
     builder.addCase(fetchAsyncDeleteLoginUser.rejected, handleError);
     builder.addCase(fetchAsyncDeleteLoginUser.pending, handleLoading);
