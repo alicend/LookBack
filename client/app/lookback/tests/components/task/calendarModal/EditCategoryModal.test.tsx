4cd09f9d536a9fc39f64d18bfef7964e8aadbab3
alicend <a.taiki623@gmail.com>
2023-10-30 22:03:23 +0900

    リファクタリング

--- client/app/lookback/tests/components/task/calendarModal/EditCategoryModal.test.tsx
+++ client/app/lookback/tests/components/task/calendarModal/EditCategoryModal.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { act, render, fireEvent, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import "@testing-library/jest-dom";
 import { RenderResult } from "@testing-library/react";
@@ -13,43 +13,49 @@ const testCategory = {
 };
 
 describe("<EditCategoryModal />", () => {
-  let getByLabelText: RenderResult["getByLabelText"];
-  let getByText: RenderResult["getByText"];
-
   beforeEach(() => {
-    const renderResult = render(
-      <Provider store={store}>
-        <ThemeProvider theme={createTheme()}>
-          <EditCategoryModal
-            open={true}
-            onClose={() => {}}
-            originalCategory={testCategory}
-          />
-        </ThemeProvider>
-      </Provider>,
-    );
-
-    getByLabelText = renderResult.getByLabelText;
-    getByText = renderResult.getByText;
+    act(() => {
+      render(
+        <Provider store={store}>
+          <ThemeProvider theme={createTheme()}>
+            <EditCategoryModal
+              open={true}
+              onClose={() => {}}
+              originalCategory={testCategory}
+            />
+          </ThemeProvider>
+        </Provider>,
+      );
+    });
   });
 
+  // ここからテストを始めます。
   test("renders EditCategoryModal component correctly", () => {
-    expect(getByLabelText("Edit category")).toHaveValue(testCategory.Category);
+    expect(screen.getByLabelText("Edit category")).toHaveValue(
+      testCategory.Category,
+    );
   });
 
   test("updates the category value correctly", () => {
-    const input = getByLabelText("Edit category");
-    fireEvent.change(input, { target: { value: "Updated Category" } });
-    expect(input).toHaveValue("Updated Category");
+    act(() => {
+      const input = screen.getByLabelText("Edit category");
+      fireEvent.change(input, { target: { value: "Updated Category" } });
+    });
+
+    expect(screen.getByLabelText("Edit category")).toHaveValue(
+      "Updated Category",
+    );
   });
 
   test("opens the delete confirmation dialog", () => {
-    const deleteButton = getByText("DELETE");
-    fireEvent.click(deleteButton);
+    act(() => {
+      const deleteButton = screen.getByText("DELETE");
+      fireEvent.click(deleteButton);
+    });
 
-    expect(getByText("Confirm Delete")).toBeInTheDocument();
+    expect(screen.getByText("Confirm Delete")).toBeInTheDocument();
     expect(
-      getByText(
+      screen.getByText(
         `カテゴリ「${testCategory.Category}」に関連するタスクも削除されますが本当に削除してよろしいですか？`,
       ),
     ).toBeInTheDocument();
